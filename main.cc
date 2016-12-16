6fd1dd2548aeb4f71d319234f2ddff0079d34da6
Takashi SASAKI <takashi316@gmail.com>
2016-12-16 20:08:48 +0900

    simple example

--- main.cc
+++ main.cc
@@ -1,13 +1,24 @@
-#include <stdlib.h>
+#include <cstdlib>
 #include "picojson/picojson.h"
 #include <fstream>
+#include <map>
+#include <sstream>
 
 
 int main(int argc, char** argv){
     std::string s;
+    std::stringstream ss;
     while(std::cin) {
        std::getline(std::cin, s);
        std::cerr << s << std::endl;
+       ss << s << std::endl;
     }
+    picojson::value picojson_value;
+    std::string error_string = picojson::parse(picojson_value, ss.str());
+    std::cerr << error_string << std::endl;
+    std::map<std::string, picojson::value> m = picojson_value.get<picojson::object>();
+    std::cerr << m["sn"] << std::endl;
+    std::cerr << m["givenName"] << std::endl;
+    std::cerr << m["twitter_id"] << std::endl;
     return EXIT_SUCCESS;
 }
